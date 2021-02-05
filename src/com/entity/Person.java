ecb74e5cf2cc5ff09d0ab1666d525991cf9d6ec8
sssiso <545426161@qq.com>
2021-02-05 11:22:23 +0800

    这是测试 git提交到本地仓库的信息

---
+++ src/com/entity/Person.java
@@ -0,0 +1,17 @@
+package com.entity;
+
+/**
+ * ClassNama:Person
+ * Package:com.entity
+ * Description:
+ *
+ * @Date:2021-02-05 11:17
+ * @Author:李淑杰
+ */
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
